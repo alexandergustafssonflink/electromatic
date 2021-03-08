5a7251ed71764e876d9168880efa509df122dd0d
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2021-03-08 14:05:53 +0100

    fixed bug?

--- electromatic/src/components/BurgerMenu/burgermenu.js
+++ electromatic/src/components/BurgerMenu/burgermenu.js
@@ -1,10 +1,11 @@
-import { stack as Menu } from "react-burger-menu";
+import { slide as Menu } from "react-burger-menu";
 import "./burgermenu.css";
+import { Link } from "@reach/router";
 
 const BurgerMenu = () => {
-  // showSettings(event) {
-  //   event.preventDefault();
-  // }
+  //   showSettings(event) {
+  //     event.preventDefault();
+  //   }
 
   // render() {
   // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
@@ -23,6 +24,21 @@ const BurgerMenu = () => {
         <h3>Connect</h3>
       </a>
     </Menu>
+
+    // <Menu right>
+    //   <Link to="/#home">
+    //     <h3>Home</h3>
+    //   </Link>
+    //   <Link to="/#events">
+    //     <h3>Events</h3>
+    //   </Link>
+    //   <Link to="/#about">
+    //     <h3>About</h3>
+    //   </Link>
+    //   <Link to="/#connect">
+    //     <h3>Connect</h3>
+    //   </Link>
+    // </Menu>
   );
 };
 export default BurgerMenu;
